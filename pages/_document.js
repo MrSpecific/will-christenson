ffe8d454b7cc452beced4308e03feba19f65b7e3
Will Christenson <will@wlcr.io>
2022-01-12 20:39:31 -0800

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="/favicon.png" />
+          <link rel="icon" href="/favicon.svg" />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
