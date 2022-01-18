0032d89ec465f4093016519210b3914fb3df69ff
Will Christenson <will@wlcr.io>
2022-01-17 23:45:05 -0800

    Improve photo loading

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,8 @@ import RenderMarkdown from '@utils/RenderMarkdown';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
 import Layout from '@components/Layout';
-import styles from '../styles/Home.module.css';
+import styles from '@styles/Home.module.css';
+import profilePhoto from '@public/images/will-portrait-28-medium.png';
 
 const { log } = console;
 
@@ -24,11 +25,12 @@ export default function Home({ intro, ...props }) {
       <main className={styles.main}>
         <div className={classNames(['triptych', 'content', 'content-y', [styles.homeContent]])}>
           <Image
-            src="/images/will-portrait-28-medium.png"
+            src={profilePhoto}
             width={1600}
             height={2200}
             alt="A black and white photo of a man standing in front of a white backdrop, wearing black clothes."
             className={styles.homePhoto}
+            placeholder="blur"
           />
           <div className="body-copy">
             <span className="drop-cap">{intro.meta.dropCap}</span>
