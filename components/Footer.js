9fb0bd306f01278702536919d1e8d9bdb5268fd3
Will Christenson <will@wlcr.io>
2022-01-31 23:01:58 -0800

    Footer Changes

--- components/Footer.js
+++ components/Footer.js
@@ -3,11 +3,18 @@ import GithubLogo from './svg/github.svg';
 import FlickrLogo from './svg/flickr.svg';
 import InstagramLogo from './svg/instagram.svg';
 
+const SocialIcon = ({ url, children, label }) => {
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer" className={styles.socialIcon}>
+      {children}
+      <span className="visually-hidden">{label}</span>
+    </a>
+  );
+};
 const SocialLink = ({ url, children, label }) => {
   return (
     <a href={url} target="_blank" rel="noopener noreferrer" className={styles.socialLink}>
-      {children}
-      <span className="visually-hidden">{label}</span>
+      {label}
     </a>
   );
 };
@@ -15,17 +22,16 @@ const SocialLink = ({ url, children, label }) => {
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <ul className={styles.socials}>
-        <SocialLink url="//github.com/MrSpecific" label="Github">
-          <GithubLogo />
-        </SocialLink>
-        <SocialLink url="//www.instagram.com/mr_specific/" label="Instagram">
-          <InstagramLogo />
-        </SocialLink>
-        <SocialLink url="//www.flickr.com/photos/bombcity/albums" label="Flickr">
-          <FlickrLogo />
-        </SocialLink>
-      </ul>
+      <div className={styles.footerSocials}>
+        <h2 className={styles.socialsHeading}>Find me on: </h2>
+        <ul className={styles.socials}>
+          <SocialLink url="//github.com/MrSpecific" label="Github" />
+          <SocialLink url="//www.linkedin.com/in/willchristenson/" label="LinkedIn" />
+          <SocialLink url="//www.polywork.com/mrspecific" label="Polywork" />
+          <SocialLink url="//www.instagram.com/mr_specific/" label="Instagram" />
+          <SocialLink url="//www.flickr.com/photos/bombcity/albums" label="Flickr" />
+        </ul>
+      </div>
     </footer>
   );
 };
