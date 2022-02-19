d228776711c76a5a1638f890f9b592cdd2a9f878
Will Christenson <will@wlcr.io>
2022-02-18 21:31:42 -0800

    Suppress Jest warnings about ESM-only modules, but not working:

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,23 @@
-module.exports = {
-  setupFilesAfterEnv: ['./jest.setup.js'],
+const nextJest = require('next/jest');
+
+// Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  // transform: {
+  //   unified: 'babel-jest',
+  // },
+  // moduleNameMapper: {
+  //   '\\unified': '<rootDir>/utils/dummy.js',
+  // },
+  testPathIgnorePatterns: ['<roodDir>/utils/parseMarkdown.js'],
+  // transformIgnorePatterns: ['<roodDir>/node_modules/(?!unified)'],
+  // transformIgnorePatterns: ['/node_modules/(?!(unified)/)'],
+  // transformIgnorePatterns: ['<roodDir>/node_modules/unified'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^@components(.*)$': '<rootDir>/components$1',
     '^@svg(.*)$': '<rootDir>/svg$1',
@@ -9,10 +27,12 @@ module.exports = {
     '^@public(.*)$': '<rootDir>/public$1',
     '^@styles(.*)$': '<rootDir>/styles$1',
     '^@utils(.*)$': '<rootDir>/utils$1',
+    '(unified)': '<rootDir>/utils/dummy.js',
+    '(remark-parse)': '<rootDir>/utils/dummy.js',
+    '(remark-gfm)': '<rootDir>/utils/dummy.js',
+    '(remark-breaks)': '<rootDir>/utils/dummy.js',
   },
-  // transformIgnorePatterns: ['/node_modules/(?!d3|d3-array|internmap|delaunator|robust-predicates)'],
-  // transformIgnorePatterns: ['/node_modules/(?!unified)'],
-  // transformIgnorePatterns: ['node_modules/?!(unified)'],
-  // transformIgnorePatterns: ['<rootDir>/(node_modules)/?!(unified)'],
-  transformIgnorePatterns: ['<roodDir>/node_modules/(?!unified)'],
+  testEnvironment: 'jest-environment-jsdom',
 };
+
+module.exports = createJestConfig(customJestConfig);
