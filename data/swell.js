245c98757637b93cdaf214b913ca94bbbb286554
Will Christenson <will@wlcr.io>
2022-05-16 15:15:27 -0700

    Shop work

---
+++ data/swell.js
@@ -0,0 +1,32 @@
+import swell from 'swell-js';
+
+const options = {
+  useCamelCase: true, // (default is false)
+};
+
+swell.init(
+  process.env.NEXT_PUBLIC_SWELL_STORE_ID,
+  process.env.NEXT_PUBLIC_SWELL_STOREFRONT_API_KEY,
+  options
+);
+
+const swellClient = {
+  init: () => {
+    swell.init(
+      process.env.NEXT_PUBLIC_SWELL_STORE_ID,
+      process.env.NEXT_PUBLIC_SWELL_STOREFRONT_API_KEY,
+      options
+    );
+  },
+  getProducts: async () => {
+    const products = await swell.products.list({
+      // category: 't-shirts',
+      limit: 100,
+      page: 1,
+      expand: ['variants'],
+    });
+    return products;
+  },
+};
+
+export { swellClient, swell };
--- pages/shop.js
+++ pages/shop.js
@@ -1,11 +1,16 @@
 import Head from 'next/head';
 import siteInfo from '@data/siteInfo';
 import { gql, request, responsiveImageFragment } from '@data/datocms';
+import { swellClient } from '@data/swell';
 import { Header, Footer, Layout } from '@components/layout';
 
 import styles from '@styles/page/Shop.module.css';
 
 export default function Shop() {
+  // swellClient.init();
+  const products = swellClient.getProducts();
+
+  console.log(products);
   return (
     <Layout>
       <Head>
@@ -15,7 +20,7 @@ export default function Shop() {
 
       <Header headline="Work" byline="Current &amp; Past Projects"></Header>
 
-      <main className="content content-y"></main>
+      <main className="content content-y">{JSON.stringify(products, null, 2)}</main>
 
       <Footer />
     </Layout>
