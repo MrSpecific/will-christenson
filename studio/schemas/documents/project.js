86ec4f851310ec33a54c9c23cbb9f178e4555c04
Will Christenson <will@wlcr.io>
2023-01-05 22:29:23 -0800

    Cleanup

--- studio/schemas/documents/project.js
+++ studio/schemas/documents/project.js
@@ -13,7 +13,7 @@ export default {
       title: 'Slug',
       type: 'slug',
       options: {
-        source: 'name',
+        source: 'title',
         maxLength: 96,
       },
     },
@@ -39,7 +39,7 @@ export default {
   ],
   preview: {
     select: {
-      title: 'name',
+      title: 'title',
       media: 'image',
     },
   },
